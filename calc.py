0bc1e4cb83bf65df1836b380b36000d22657755c
Faithful Pursuer <abdullahdk690@gmail.com>
2025-07-31 20:13:06 +0600

    added some new features to calc.py

--- calc.py
+++ calc.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+import math as m
 
 class App(ctk.CTk):
 
@@ -36,9 +37,9 @@ class App(ctk.CTk):
         self.button_value = []
         
         self.title("My Own Calculator")
-        self.geometry("330x430")
-        self.maxsize(330, 430)
-        self.minsize(330, 430)
+        # self.geometry("330x430")
+        self.maxsize(330, 530)
+        self.minsize(330, 530)
         self.grid_columnconfigure((0, 1, 2, 3), weight=1)
         self.grid_rowconfigure(0, weight=1)
 
@@ -47,7 +48,7 @@ class App(ctk.CTk):
         self.frame.grid(row=0, column=0, padx=20, pady=(70, 20), sticky = "nsew", columnspan=3)
 
         self.frame.grid_columnconfigure((0, 1, 2, 3), weight=1)
-        self.frame.grid_rowconfigure((0, 1, 2, 3, 4), weight=1)
+        self.frame.grid_rowconfigure((0, 1, 2, 3, 4, 5, 6, 7), weight=1)
 
 #result labet
         self.result = ctk.CTkLabel(self, text = "Result: ", font=("Arial", 16))
@@ -105,10 +106,10 @@ class App(ctk.CTk):
         self.button_mul = ctk.CTkButton(self.frame, text = "÷", border_width = 2, fg_color ="#E96C5C", font=("Arial", 20), corner_radius = 20, hover_color = "#D23648", command = lambda: [self.insert("÷"), self.insert_list("/")])
         self.button_mul.grid(row = 3, column = 3, padx = 10, pady = 10, sticky = "nsew", columnspan = 1, rowspan = 1)
 #================== row 4
-        self.button_sqr = ctk.CTkButton(self.frame, text = "^2", corner_radius = 10, font=("Arial", 15), border_width = 2, command = lambda: [self.insert("^2"), self.insert_list("**2")])
+        self.button_sqr = ctk.CTkButton(self.frame, text = "x²", corner_radius = 10, font=("Arial", 15), border_width = 2, command = lambda: [self.insert("²"), self.insert_list("**2")])
         self.button_sqr.grid(row = 4, column = 0, padx = 10, pady = 10, sticky = "nsew", columnspan = 1, rowspan = 1)
 
-        self.button_sqrt = ctk.CTkButton(self.frame, text = "√", corner_radius = 10, font=("Arial", 15), border_width = 2, command = lambda: [self.insert("√"), self.insert_list("**0.5")])
+        self.button_sqrt = ctk.CTkButton(self.frame, text = "√", corner_radius = 10, font=("Arial", 15), border_width = 2, command = lambda: [self.insert("√("), self.insert_list("m.sqrt(")])
         self.button_sqrt.grid(row = 4, column = 1, padx = 10, pady = 10, sticky = "nsew", columnspan = 1, rowspan = 1)
 
         self.button_Del = ctk.CTkButton(self.frame, text = "Del", border_width = 2, fg_color = "#E54E45", font=("Arial", 15), corner_radius = 20, hover_color = "#DD2161", command = self.Del)
@@ -116,6 +117,44 @@ class App(ctk.CTk):
 
         self.button_AC = ctk.CTkButton(self.frame, text = "AC", border_width = 2, fg_color ="#E44A40", font=("Arial", 15), corner_radius = 20, hover_color = "#DD2161", command = self.AC)
         self.button_AC.grid(row = 4, column = 3, padx = 10, pady = 10, sticky = "nsew", columnspan = 1, rowspan = 1)
+#========================= row 5
+        self.button_sqr = ctk.CTkButton(self.frame, text = "x³", corner_radius = 10, font=("Arial", 15), border_width = 2, command = lambda: [self.insert("³"), self.insert_list("**3")])
+        self.button_sqr.grid(row = 5, column = 0, padx = 10, pady = 10, sticky = "nsew", columnspan = 1, rowspan = 1)
+
+        self.button_sqrt = ctk.CTkButton(self.frame, text = "∛", corner_radius = 10, font=("Arial", 15), border_width = 2, command = lambda: [self.insert("∛("), self.insert_list("m.cbrt(")])
+        self.button_sqrt.grid(row = 5, column = 1, padx = 10, pady = 10, sticky = "nsew", columnspan = 1, rowspan = 1)
+
+        self.button_sqr = ctk.CTkButton(self.frame, text = "(", corner_radius = 10, font=("Arial", 15), border_width = 2, command = lambda: [self.insert("("), self.insert_list("(")])
+        self.button_sqr.grid(row = 5, column = 2, padx = 10, pady = 10, sticky = "nsew", columnspan = 1, rowspan = 1)
+
+        self.button_sqrt = ctk.CTkButton(self.frame, text = ")", corner_radius = 10, font=("Arial", 15), border_width = 2, command = lambda: [self.insert(")"), self.insert_list(")")])
+        self.button_sqrt.grid(row = 5, column = 3, padx = 10, pady = 10, sticky = "nsew", columnspan = 1, rowspan = 1)
+#========================= row 6
+        self.button_sqr = ctk.CTkButton(self.frame, text = "xʸ", corner_radius = 10, font=("Arial", 15), border_width = 2, command = lambda: [self.insert("^("), self.insert_list("**(")])
+        self.button_sqr.grid(row = 6, column = 0, padx = 10, pady = 10, sticky = "nsew", columnspan = 1, rowspan = 1)
+
+        self.button_sqrt = ctk.CTkButton(self.frame, text = "x⁻¹", corner_radius = 10, font=("Arial", 15), border_width = 2, command = lambda: [self.insert("^-1"), self.insert_list("** -1")])
+        self.button_sqrt.grid(row = 6, column = 1, padx = 10, pady = 10, sticky = "nsew", columnspan = 1, rowspan = 1)
+
+        self.button_sqr = ctk.CTkButton(self.frame, text = "!", corner_radius = 10, font=("Arial", 15), border_width = 2, command = lambda: [self.insert("!("), self.insert_list("m.factorial(")])
+        self.button_sqr.grid(row = 6, column = 2, padx = 10, pady = 10, sticky = "nsew", columnspan = 1, rowspan = 1)
+
+        self.button_sqrt = ctk.CTkButton(self.frame, text = "log", corner_radius = 10, font=("Arial", 15), border_width = 2, command = lambda: [self.insert("log("), self.insert_list("m.log10(")])
+        self.button_sqrt.grid(row = 6, column = 3, padx = 10, pady = 10, sticky = "nsew", columnspan = 1, rowspan = 1)
+#========================= row 7
+        self.button_sqr = ctk.CTkButton(self.frame, text = "sin", corner_radius = 10, font=("Arial", 15), border_width = 2, command = lambda: [self.insert("sin("), self.insert_list("m.sin(")])
+        self.button_sqr.grid(row = 7, column = 0, padx = 10, pady = 10, sticky = "nsew", columnspan = 1, rowspan = 1)
+
+        self.button_sqrt = ctk.CTkButton(self.frame, text = "cos", corner_radius = 10, font=("Arial", 15), border_width = 2, command = lambda: [self.insert("cos("), self.insert_list("m.cos(")])
+        self.button_sqrt.grid(row = 7, column = 1, padx = 10, pady = 10, sticky = "nsew", columnspan = 1, rowspan = 1)
+
+        self.button_sqr = ctk.CTkButton(self.frame, text = "tan", corner_radius = 10, font=("Arial", 15), border_width = 2, command = lambda: [self.insert("tan("), self.insert_list("m.tan(")])
+        self.button_sqr.grid(row = 7, column = 2, padx = 10, pady = 10, sticky = "nsew", columnspan = 1, rowspan = 1)
+
+        self.button_sqrt = ctk.CTkButton(self.frame, text = "π", corner_radius = 10, font=("Arial", 15), border_width = 2, command = lambda: [self.insert("π"), self.insert_list("m.pi")])
+        self.button_sqrt.grid(row = 7, column = 3, padx = 10, pady = 10, sticky = "nsew", columnspan = 1, rowspan = 1)
+
+
 
 
 app = App()
